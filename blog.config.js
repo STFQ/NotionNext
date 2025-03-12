bd36593dbbe65555b34d644a75fe1f7388ec4287
cavalier <1256895841@qq.com>
2025-03-12 14:38:46 +0800

    提交修改作者名

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: 'Cavalier', // 您的昵称 例如 tangly1024
+  AUTHOR: 'Lior', // 您的昵称 例如 tangly1024
   BIO: '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: 'HYH, Talking', // 网站关键词 英文逗号隔开
